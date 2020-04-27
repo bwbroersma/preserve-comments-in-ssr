0e873e990e9407984fb4fdf3e021912121b890b6
Benjamin W. Broersma <bw@broersma.com>
2020-04-27 15:20:52 +0200

    Changes from svelte/template to show preserveComments does not work.

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <main>
+	<!-- some comment to preserve -->
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
